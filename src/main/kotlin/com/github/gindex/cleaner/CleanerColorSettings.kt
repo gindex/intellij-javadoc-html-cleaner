81e419364cb41018ec9268e7a8972febf4a54aef
Yevgen Pikus <yevgen.pikus@gmail.com>
2019-11-12 21:51:19 +0100

    Add configuration support for more tags

--- src/main/kotlin/com/github/gindex/cleaner/CleanerColorSettings.kt
+++ src/main/kotlin/com/github/gindex/cleaner/CleanerColorSettings.kt
@@ -15,19 +15,38 @@ import javax.swing.Icon
 class CleanerColorSettings : ColorSettingsPage {
     companion object {
         val boldStyleKey = createTextAttributesKey("BOLD_FONT", HighlighterColors.TEXT)
+        val strongStyleKey = createTextAttributesKey("STRONG_FONT", HighlighterColors.TEXT)
+        val emStyleKey = createTextAttributesKey("EM_FONT", HighlighterColors.TEXT)
+        val h1StyleKey = createTextAttributesKey("H1_FONT", HighlighterColors.TEXT)
+        val h2StyleKey = createTextAttributesKey("H2_FONT", HighlighterColors.TEXT)
+        val h3StyleKey = createTextAttributesKey("H3_FONT", HighlighterColors.TEXT)
+        val h4StyleKey = createTextAttributesKey("H4_FONT", HighlighterColors.TEXT)
+        val h5StyleKey = createTextAttributesKey("H5_FONT", HighlighterColors.TEXT)
+        val h6StyleKey = createTextAttributesKey("H6_FONT", HighlighterColors.TEXT)
+        val preStyleKey = createTextAttributesKey("PRE_FONT", HighlighterColors.TEXT)
+        val linkStyleKey = createTextAttributesKey("LINK_FONT", HighlighterColors.TEXT)
+        val codeStyleKey = createTextAttributesKey("CODE_FONT", HighlighterColors.TEXT)
         val italicStyleKey = createTextAttributesKey("ITALIC_FONT", HighlighterColors.TEXT)
+        val citeStyleKey = createTextAttributesKey("CITE_FONT", HighlighterColors.TEXT)
+        val teletypeStyleKey = createTextAttributesKey("TT_FONT", HighlighterColors.TEXT)
+
 
         private val ATTRS = arrayOf(
-            AttributesDescriptor("<b>", boldStyleKey),
-            AttributesDescriptor("<strong>", boldStyleKey),
-            AttributesDescriptor("<pre>", boldStyleKey),
-            AttributesDescriptor("<em>", boldStyleKey),
-            AttributesDescriptor("<h1..6>", boldStyleKey),
-            AttributesDescriptor("<a href= ..> | <a name= ..>", boldStyleKey),
-            AttributesDescriptor("<i>", italicStyleKey),
-            AttributesDescriptor("<code>", italicStyleKey),
-            AttributesDescriptor("<cite>", italicStyleKey),
-            AttributesDescriptor("<tt>", italicStyleKey)
+                AttributesDescriptor("<b>", boldStyleKey),
+                AttributesDescriptor("<strong>", strongStyleKey),
+                AttributesDescriptor("<pre>", preStyleKey),
+                AttributesDescriptor("<em>", emStyleKey),
+                AttributesDescriptor("<a ..>", linkStyleKey),
+                AttributesDescriptor("<i>", italicStyleKey),
+                AttributesDescriptor("<code>", codeStyleKey),
+                AttributesDescriptor("<cite>", citeStyleKey),
+                AttributesDescriptor("<tt>", teletypeStyleKey),
+                AttributesDescriptor("<h1>", h1StyleKey),
+                AttributesDescriptor("<h2>", h2StyleKey),
+                AttributesDescriptor("<h3>", h3StyleKey),
+                AttributesDescriptor("<h4>", h4StyleKey),
+                AttributesDescriptor("<h5>", h5StyleKey),
+                AttributesDescriptor("<h6>", h6StyleKey)
         )
 
     }
@@ -38,8 +57,21 @@ class CleanerColorSettings : ColorSettingsPage {
 
     override fun getAdditionalHighlightingTagToDescriptorMap(): MutableMap<String, TextAttributesKey> {
         return mapOf(
-            "bold" to boldStyleKey,
-            "italic" to italicStyleKey
+                "bold" to boldStyleKey,
+                "strong" to strongStyleKey,
+                "em" to emStyleKey,
+                "h1" to h1StyleKey,
+                "h2" to h2StyleKey,
+                "h3" to h3StyleKey,
+                "h4" to h4StyleKey,
+                "h5" to h5StyleKey,
+                "h6" to h6StyleKey,
+                "pre" to preStyleKey,
+                "link" to linkStyleKey,
+                "code" to codeStyleKey,
+                "italic" to italicStyleKey,
+                "cite" to citeStyleKey,
+                "teletype" to teletypeStyleKey
         ).toMutableMap()
     }
 
--- src/main/kotlin/com/github/gindex/cleaner/Tags.kt
+++ src/main/kotlin/com/github/gindex/cleaner/Tags.kt
@@ -1,7 +1,20 @@
 package com.github.gindex.cleaner
 
 import com.github.gindex.cleaner.CleanerColorSettings.Companion.boldStyleKey
+import com.github.gindex.cleaner.CleanerColorSettings.Companion.citeStyleKey
+import com.github.gindex.cleaner.CleanerColorSettings.Companion.codeStyleKey
+import com.github.gindex.cleaner.CleanerColorSettings.Companion.emStyleKey
+import com.github.gindex.cleaner.CleanerColorSettings.Companion.h1StyleKey
+import com.github.gindex.cleaner.CleanerColorSettings.Companion.h2StyleKey
+import com.github.gindex.cleaner.CleanerColorSettings.Companion.h3StyleKey
+import com.github.gindex.cleaner.CleanerColorSettings.Companion.h4StyleKey
+import com.github.gindex.cleaner.CleanerColorSettings.Companion.h5StyleKey
+import com.github.gindex.cleaner.CleanerColorSettings.Companion.h6StyleKey
 import com.github.gindex.cleaner.CleanerColorSettings.Companion.italicStyleKey
+import com.github.gindex.cleaner.CleanerColorSettings.Companion.linkStyleKey
+import com.github.gindex.cleaner.CleanerColorSettings.Companion.preStyleKey
+import com.github.gindex.cleaner.CleanerColorSettings.Companion.strongStyleKey
+import com.github.gindex.cleaner.CleanerColorSettings.Companion.teletypeStyleKey
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import java.util.regex.Matcher
 import java.util.regex.Pattern
@@ -9,20 +22,20 @@ import java.util.regex.Pattern
 open class Tag(name: String) {
     companion object {
         private val BOLD = BoldTag("b")
-        private val STRONG = BoldTag("strong")
-        private val EM = BoldTag("em")
-        private val H1 = BoldTag("h1")
-        private val H2 = BoldTag("h2")
-        private val H3 = BoldTag("h3")
-        private val H4 = BoldTag("h4")
-        private val H5 = BoldTag("h5")
-        private val H6 = BoldTag("h6")
+        private val STRONG = StrongTag("strong")
+        private val EM = EmTag("em")
+        private val H1 = H1Tag("h1")
+        private val H2 = H2Tag("h2")
+        private val H3 = H3Tag("h3")
+        private val H4 = H4Tag("h4")
+        private val H5 = H5Tag("h5")
+        private val H6 = H16Tag("h6")
         private val PRE = PreTag("pre")
         private val LINK = LinkTag("a")
         private val CODE = CodeTag("code")
         private val ITALIC = ItalicTag("i")
-        private val CITE = ItalicTag("cite")
-        private val TELETYPE = ItalicTag("tt")
+        private val CITE = CiteTag("cite")
+        private val TELETYPE = TeletypeTag("tt")
 
         val tags = listOf(BOLD, STRONG, EM, PRE, H1, H2, H3, H4, H5, H6, LINK, CODE, ITALIC, CITE, TELETYPE)
 
@@ -34,19 +47,39 @@ open class Tag(name: String) {
 }
 
 class BoldTag(name: String) : Tag(name)
-class ItalicTag(name: String) : Tag(name)
+class StrongTag(name: String) : Tag(name)
+class EmTag(name: String) : Tag(name)
+class H1Tag(name: String) : Tag(name)
+class H2Tag(name: String) : Tag(name)
+class H3Tag(name: String) : Tag(name)
+class H4Tag(name: String) : Tag(name)
+class H5Tag(name: String) : Tag(name)
+class H16Tag(name: String) : Tag(name)
 class PreTag(name: String) : Tag(name)
 class LinkTag(name: String) : Tag(name)
 class CodeTag(name: String) : Tag(name)
+class ItalicTag(name: String) : Tag(name)
+class CiteTag(name: String) : Tag(name)
+class TeletypeTag(name: String) : Tag(name)
 
 object TagStyle {
     fun mapTagToStyle(tag: Tag): TextAttributesKey? {
         return when (tag) {
             is BoldTag -> boldStyleKey
+            is StrongTag -> strongStyleKey
+            is EmTag -> emStyleKey
+            is H1Tag -> h1StyleKey
+            is H2Tag -> h2StyleKey
+            is H3Tag -> h3StyleKey
+            is H4Tag -> h4StyleKey
+            is H5Tag -> h5StyleKey
+            is H16Tag -> h6StyleKey
+            is PreTag -> preStyleKey
+            is LinkTag -> linkStyleKey
+            is CodeTag -> codeStyleKey
             is ItalicTag -> italicStyleKey
-            is PreTag -> boldStyleKey
-            is LinkTag -> boldStyleKey
-            is CodeTag -> italicStyleKey
+            is CiteTag -> citeStyleKey
+            is TeletypeTag -> teletypeStyleKey
             else -> null
         }
     }

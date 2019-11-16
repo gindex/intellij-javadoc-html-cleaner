4768302fad844091f30a5ae4b8e73b32687c40a3
Yevgen Pikus <yevgen.pikus@gmail.com>
2019-11-16 18:56:15 +0100

    Add add demo text

--- src/main/kotlin/com/github/gindex/cleaner/CleanerColorSettings.kt
+++ src/main/kotlin/com/github/gindex/cleaner/CleanerColorSettings.kt
@@ -55,7 +55,7 @@ class CleanerColorSettings : ColorSettingsPage {
 
     override fun getAdditionalHighlightingTagToDescriptorMap(): MutableMap<String, TextAttributesKey> {
         return mapOf(
-                "bold" to boldStyleKey,
+                "b" to boldStyleKey,
                 "strong" to strongStyleKey,
                 "em" to emStyleKey,
                 "h1" to h1StyleKey,
@@ -67,7 +67,7 @@ class CleanerColorSettings : ColorSettingsPage {
                 "pre" to preStyleKey,
                 "link" to linkStyleKey,
                 "code" to codeStyleKey,
-                "italic" to italicStyleKey,
+                "i" to italicStyleKey,
                 "cite" to citeStyleKey,
                 "teletype" to teletypeStyleKey
         ).toMutableMap()
@@ -90,7 +90,27 @@ class CleanerColorSettings : ColorSettingsPage {
     }
 
     override fun getDemoText(): String {
-        return "My demo: <code> this.toString() </code>"
+        return """
+        |/**
+        | * This is a test comment
+        | *
+        | * Test for bold: <b>bold</b>
+        | * Test for strong: <strong>strong</strong>
+        | * Test for em: <em>em</em>
+        | * Test for h1: <h1>h1</h1>
+        | * Test for h2: <h2>h2</h2>
+        | * Test for h3: <h3>h3</h3>
+        | * Test for h4: <h4>h4</h4>
+        | * Test for h5: <h5>h5</h5>
+        | * Test for h6: <h6>h6</h6>
+        | * Test for pre: <pre>pre</pre>
+        | * Test for link: <link>link</link>
+        | * Test for code: <code>code</code>
+        | * Test for italic: <i>italic</i>
+        | * Test for cite: <cite>cite</cite>
+        | * Test for teletype: <teletype>teletype</teletype>
+        | */ 
+        """.trimMargin()
     }
 
 }

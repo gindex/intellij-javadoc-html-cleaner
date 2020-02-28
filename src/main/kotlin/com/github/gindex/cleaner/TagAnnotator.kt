e6ed69e7de9703d234017c12431f42ef8e667e46
Yevgen Pikus <yevgen.pikus@gmail.com>
2020-02-28 23:22:41 +0100

    Use AnnotationBuilder because of deprecation

--- src/main/kotlin/com/github/gindex/cleaner/TagAnnotator.kt
+++ src/main/kotlin/com/github/gindex/cleaner/TagAnnotator.kt
@@ -2,6 +2,7 @@ package com.github.gindex.cleaner
 
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
+import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.psi.javadoc.PsiDocComment
@@ -23,7 +24,10 @@ class TagAnnotator : Annotator {
                         extractRangesWithSplit(text, javadoc.textOffset + start, javadoc.textOffset + end)
                     }
                     .forEach { range ->
-                        holder.createInfoAnnotation(range, null).textAttributes = tagStyleKey
+                        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                                .range(range)
+                                .textAttributes(tagStyleKey)
+                                .create()
                     }
         }
     }
